baaa2df7d35c1e5d00d8f9c56af93e4fb00b769a
Chris Lemke <spcbrn1@gmail.com>
2017-08-11 10:10:05 -0600

    added completed assessment files

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,61 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt:(string) => {
+    return string.split('').reverse().join('');
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups:(array) => {
+    return array.filter((obj, idx, arr) => {
+      return arr.indexOf(obj) === idx;
+    })
+  },
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+  titleIt:(string) => {
+    let strArr = string.split(' ');
+    let output = [];
+    strArr.forEach(word => {
+      let f = word.charAt(0);
+      output.push(f.toUpperCase() + word.slice(1));
+    });
+    return output.join(' ');
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+  vowelCounter:(string) => {
+    let strArr = string.split('');
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    strArr.forEach(item => {
+      if (vowels.includes(item)) {
+        count++
+      }
+    })
+    return count;
+  },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+  isPrime:(num) => {
+  if (num <=0) {
+    return false
+  }
+  for (i=2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +64,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo: '1020',
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +75,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
+  log1: 'Hello World',
+  log2: undefined
 }
